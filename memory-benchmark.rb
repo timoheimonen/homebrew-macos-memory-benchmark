34659f1e81c7b157407602c8d0526bdf205cb749
Timo Heimonen <timo.heimonen@proton.me>
2025-12-27 12:23:34 +0200

    Update memory-benchmark to version 0.51

--- memory-benchmark.rb
+++ memory-benchmark.rb
@@ -4,8 +4,8 @@
 class MemoryBenchmark < Formula
   desc "Memory performance benchmark tool for macOS Apple Silicon"
   homepage "https://github.com/timoheimonen/macOS-memory-benchmark"
-  url "https://github.com/timoheimonen/macOS-memory-benchmark/archive/refs/tags/v.0.50.tar.gz"
-  sha256 "6a7cf412ff6322be5e62e185a4a9fdd1e68e3e9eb24411c3ce4656f4554afc01"
+  url "https://github.com/timoheimonen/macOS-memory-benchmark/archive/refs/tags/v.0.51.tar.gz"
+  sha256 "f5fd7a57a6ace37f600f815bd06ea233d7635ec7fc4889142352d1357dba15f3"
   license "GPL-3.0"
   head "https://github.com/timoheimonen/macOS-memory-benchmark.git", branch: "main"
 
