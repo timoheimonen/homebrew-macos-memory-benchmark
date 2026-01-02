62d54dfe5361be1b67c7542a3016625a4cc0dc14
Timo Heimonen <timo.heimonen@proton.me>
2026-01-02 12:35:07 +0200

    Update sha256 checksum for memory-benchmark.rb

--- memory-benchmark.rb
+++ memory-benchmark.rb
@@ -5,7 +5,7 @@ class MemoryBenchmark < Formula
   desc "Memory performance benchmark tool for macOS Apple Silicon"
   homepage "https://github.com/timoheimonen/macOS-memory-benchmark"
   url "https://github.com/timoheimonen/macOS-memory-benchmark/archive/refs/tags/v.0.52.3.tar.gz"
-  sha256 "41816211b1bbb5a8189db0df8cb4b3a020dbb538cb3f56382e814f25393b93b9"
+  sha256 "7590105b3752163a923415c7e5d1adf3d76e615a5ddacd3af225a9d7f1e2545b"
   license "GPL-3.0"
   head "https://github.com/timoheimonen/macOS-memory-benchmark.git", branch: "main"
 
